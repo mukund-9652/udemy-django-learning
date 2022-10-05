be2edb9ddfc0dd5bdb487722ed489e1167ba2ce8
mukund-9652 <sit19cs040@sairamtap.edu.in>
2022-10-05 23:52:02 +0530

    admin dashboard updated commit 11:51pm 04-10-2022

--- btre/urls.py
+++ btre/urls.py
@@ -1,3 +1,4 @@
+
 """btre URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -15,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include 
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('pages.urls')),
+    path('listings/', include('listings.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
